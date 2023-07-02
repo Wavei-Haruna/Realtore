d6f978acc2175757405df53c3f834dc3ac63fef8
wavei-haruna <wavharun2@gmail.com>
2023-07-02 20:53:52 +0000

    I have added the the routes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import Offers from './Pages/Offers'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import ForgetPassword from './Pages/ForgetPassword'
+
+export default function App() {
+  return (
+    < >
+
+      <Router>
+
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route  path='/profile' element={<Profile/>}/>
+        <Route  path='/offers' element={<Offers/>}/>
+        <Route  path='/sign-in' element={<SignIn/>}/>
+        <Route  path='/sign-up' element={<SignUp/>}/>
+        <Route  path='/forget-password' element={<ForgetPassword/>}/>
+      </Routes>
+      </Router>
+    </>
+  )
+}
